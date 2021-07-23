7eac72a41bf9b12450a61ac26297b9f05fc7744b
서해승 <hsseo1029@naver.com>
2021-07-23 12:06:29 +0900

    a

---
+++ cstudy2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main() {
+	int array[5];
+	int i;
+	int high, avg;
+	int sum = 0;
+	for(i = 0; i < 5; i++) {
+		scanf("%d", &array[i]);
+		sum += array[i];
+	}
+	avg = sum / 5;
+	high = 0;
+	for(i = 0; i < 5; i++) {
+		if(array[i] > high) {
+			high = array[i];
+		}
+	}
+	printf("평균 : %d\n", avg);
+	printf("최고 : %d", high);
+}
